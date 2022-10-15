d43d1c9e09787d62e103c186aff0ea6774119d0e
Fabiano Andrade Martins da Costa <115885819+AMCFabiano@users.noreply.github.com>
2022-10-15 12:57:48 -0300

    Initial commit

---
+++ ex010-JSbasico/js/meuScript.js
@@ -0,0 +1,15 @@
+//window.alert("ola mundo")
+window.console.log("teste")
+window.console.log(
+    window.document.getElementById("quadro01").innerHTML
+    )
+
+//window.console.log(window)
+//window.console.log(navigator.appCodeName)
+
+
+function retornar(){
+    history.back()
+}
+
+window.console.log(window.document.getElementById("frutas"))
